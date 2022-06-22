d745dd1977b2a89a7fd216fb31e966cb0ebc560f
Darnell Andries <dandries@brave.com>
2022-06-21 22:33:00 -0700

    Rename metric log store and p3a config, move MessageMetainfo init/update functions to class

--- components/p3a/p3a_message.h
+++ components/p3a/p3a_message.h
@@ -12,14 +12,23 @@
 #include "base/time/time.h"
 #include "base/values.h"
 
+class PrefService;
+
 namespace brave {
 
 constexpr const char* kP3AMessageStarKeyValueSeparator = "|";
 constexpr const char* kP3AMessageStarLayerSeparator = ";";
 
-struct MessageMetainfo {
+class MessageMetainfo {
+ public:
   MessageMetainfo();
   ~MessageMetainfo();
+
+  void Init(PrefService* local_state,
+            std::string channel,
+            std::string week_of_install);
+
+  void Update();
 
   std::string platform;
   std::string version;
@@ -30,6 +39,11 @@ struct MessageMetainfo {
   int wos;  // Week of survey.
   std::string country_code;
   std::string refcode;
+
+ private:
+  // Ensures that country/refcode represent the big enough cohort that will not
+  // let anybody identify the sender.
+  void MaybeStripRefcodeAndCountry();
 };
 
 base::Value GenerateP3AMessageDict(base::StringPiece metric_name,
@@ -39,10 +53,6 @@ base::Value GenerateP3AMessageDict(base::StringPiece metric_name,
 std::string GenerateP3AStarMessage(base::StringPiece metric_name,
                                    uint64_t metric_value,
                                    const MessageMetainfo& meta);
-
-// Ensures that country/refcode represent the big enough cohort that will not
-// let anybody identify the sender.
-void MaybeStripRefcodeAndCountry(MessageMetainfo* meta);
 
 }  // namespace brave
 
