95ecb5835eba70569a1b399c39303bfedde3878f
Deep <deeppandya91@gmail.com>
2022-08-02 01:28:52 -0400

    Update flow with desktop receipt

--- components/brave_vpn/brave_vpn_service.h
+++ components/brave_vpn/brave_vpn_service.h
@@ -155,9 +155,7 @@ class BraveVpnService :
                            const std::string& product_id,
                            const std::string& product_type,
                            const std::string& bundle_id);
-  void GetSubscriberCredentialV12(ResponseCallback callback,
-                                  const std::string& payments_environment,
-                                  const std::string& monthly_pass);
+  void GetSubscriberCredentialV12(ResponseCallback callback);
 
  private:
   friend class BraveVPNServiceTest;
